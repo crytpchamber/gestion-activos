17cb7a6f9198f6b3ee885c66b480f8f0ad273d90
cryptchamber <cryptchamber@gmail.com>
2016-11-22 09:02:49 -0400

    Inclusion de reportes y Validaciones de pantallas, y campos

--- conf/database.sql
+++ conf/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-11-2016 a las 14:31:18
+-- Tiempo de generación: 22-11-2016 a las 14:01:49
 -- Versión del servidor: 10.1.16-MariaDB
 -- Versión de PHP: 5.6.24
 
@@ -30,7 +30,7 @@ USE `mydb`;
 --
 
 DROP TABLE IF EXISTS `activos`;
-CREATE TABLE `activos` (
+CREATE TABLE IF NOT EXISTS `activos` (
   `idActivos` int(11) NOT NULL,
   `Descripcion` varchar(45) NOT NULL,
   `fecha_adquisicion` date NOT NULL,
@@ -38,20 +38,25 @@ CREATE TABLE `activos` (
   `valor_adquisicion` decimal(18,3) NOT NULL DEFAULT '0.000',
   `fecha_registro` date NOT NULL,
   `fecha_ini_deprec` date NOT NULL,
+  `codActivo` varchar(100) NOT NULL,
   `serial` varchar(45) NOT NULL,
+  `estado` char(1) NOT NULL,
   `ubicacion_idUbicacion` int(11) NOT NULL,
   `categorias_idCategoria` int(11) NOT NULL,
   `idSubCategoria` int(11) NOT NULL,
-  `estado` varchar(2) NOT NULL DEFAULT 'D'
+  PRIMARY KEY (`idActivos`),
+  KEY `fk_activos_ubicacion1_idx` (`ubicacion_idUbicacion`),
+  KEY `Categoria_idCategoria` (`categorias_idCategoria`),
+  KEY `idSubCategoria` (`idSubCategoria`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `activos`
 --
 
-INSERT INTO `activos` (`idActivos`, `Descripcion`, `fecha_adquisicion`, `tiempo_depre`, `valor_adquisicion`, `fecha_registro`, `fecha_ini_deprec`, `serial`, `ubicacion_idUbicacion`, `categorias_idCategoria`, `idSubCategoria`,`estado`) VALUES
-(1, 'Laptop Dell E6510', '2016-03-15', 6, '250001.000', '2016-10-07', '2011-04-14', '', 2, 1, 1,'D'),
-(2, 'Boligrafo Azul', '2016-10-07', 3, '1050.000', '2016-10-07', '2016-10-07', '', 2, 3, 2,'D');
+INSERT INTO `activos` (`idActivos`, `Descripcion`, `fecha_adquisicion`, `tiempo_depre`, `valor_adquisicion`, `fecha_registro`, `fecha_ini_deprec`, `codActivo`, `serial`, `estado`, `ubicacion_idUbicacion`, `categorias_idCategoria`, `idSubCategoria`) VALUES
+(1, 'Laptop Dell E6510', '2016-03-15', 6, '250001.000', '2016-10-07', '2011-04-14', '', '', 'A', 2, 2, 1),
+(2, 'prueba', '2016-11-13', 3, '123123.000', '2016-11-13', '2016-11-13', '1luyhq', '12345', 'A', 1, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -60,9 +65,10 @@ INSERT INTO `activos` (`idActivos`, `Descripcion`, `fecha_adquisicion`, `tiempo_
 --
 
 DROP TABLE IF EXISTS `categorias`;
-CREATE TABLE `categorias` (
+CREATE TABLE IF NOT EXISTS `categorias` (
   `idCategoria` int(11) NOT NULL,
-  `Descripcion` varchar(45) NOT NULL
+  `Descripcion` varchar(45) NOT NULL,
+  PRIMARY KEY (`idCategoria`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -72,7 +78,8 @@ CREATE TABLE `categorias` (
 INSERT INTO `categorias` (`idCategoria`, `Descripcion`) VALUES
 (1, 'Maquinarias'),
 (2, 'Teconologicos'),
-(3, 'Articulos de Oficina');
+(3, 'Articulos de Oficina'),
+(4, 'Servicios Generales');
 
 -- --------------------------------------------------------
 
@@ -81,12 +88,13 @@ INSERT INTO `categorias` (`idCategoria`, `Descripcion`) VALUES
 --
 
 DROP TABLE IF EXISTS `mapas_acceso`;
-CREATE TABLE `mapas_acceso` (
+CREATE TABLE IF NOT EXISTS `mapas_acceso` (
   `idmapas_acceso` int(11) NOT NULL,
   `Descripcion` varchar(45) DEFAULT NULL,
   `puedeEliminar` tinyint(1) DEFAULT NULL,
   `puedeModificar` tinyint(1) DEFAULT NULL,
-  `puedeGuardar` tinyint(1) DEFAULT NULL
+  `puedeGuardar` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`idmapas_acceso`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -104,7 +112,7 @@ INSERT INTO `mapas_acceso` (`idmapas_acceso`, `Descripcion`, `puedeEliminar`, `p
 --
 
 DROP TABLE IF EXISTS `modulos`;
-CREATE TABLE `modulos` (
+CREATE TABLE IF NOT EXISTS `modulos` (
   `idmodulos` int(11) NOT NULL,
   `descModulo` varchar(45) DEFAULT NULL,
   `mapas_acceso_idmapas_acceso` int(11) NOT NULL,
@@ -112,7 +120,9 @@ CREATE TABLE `modulos` (
   `sucursal` tinyint(4) DEFAULT '0',
   `activos` tinyint(4) DEFAULT '0',
   `responsable` tinyint(4) DEFAULT '0',
-  `categorias` int(11) NOT NULL
+  `categorias` int(11) NOT NULL,
+  PRIMARY KEY (`idmodulos`),
+  KEY `fk_modulos_mapas_acceso1_idx` (`mapas_acceso_idmapas_acceso`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -130,13 +140,14 @@ INSERT INTO `modulos` (`idmodulos`, `descModulo`, `mapas_acceso_idmapas_acceso`,
 --
 
 DROP TABLE IF EXISTS `pistasauditoria`;
-CREATE TABLE `pistasauditoria` (
+CREATE TABLE IF NOT EXISTS `pistasauditoria` (
   `idpistasAuditoria` int(11) NOT NULL,
   `fechaPista` date DEFAULT NULL,
   `tipo_operacion` char(1) DEFAULT NULL,
   `modulo` varchar(45) DEFAULT NULL,
   `observacion` varchar(255) DEFAULT NULL,
-  `usuario` int(11) DEFAULT NULL
+  `usuario` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idpistasAuditoria`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -158,7 +169,51 @@ INSERT INTO `pistasauditoria` (`idpistasAuditoria`, `fechaPista`, `tipo_operacio
 (29, '2016-10-29', 'M', 'Activos', 'Se modifico la fecha de adquisicion de 2016-10-26 -> 2016-10-25. ', 41),
 (30, '2016-10-29', 'I', 'Gestion de Activos', 'se registro la Asignacion de Activo: 2', 41),
 (31, '2016-10-29', 'E', 'Gestion de Activos', 'se elimino la Asignacion de Activo: 2', 41),
-(32, '2016-10-29', 'E', 'Activos', 'se elimino el Activo: 3', 41);
+(32, '2016-10-29', 'E', 'Activos', 'se elimino el Activo: 3', 41),
+(33, '2016-11-03', 'E', 'Activos', 'se elimino el Activo: 2', 41),
+(34, '2016-11-03', 'I', 'Activos', 'se registro el activo: 123456', 41),
+(35, '2016-11-03', 'I', 'Activos', 'se registro el activo: 1234567', 41),
+(36, '2016-11-03', 'E', 'Sucursales', 'se elimino la Sucursal: 3', 41),
+(37, '2016-11-07', 'I', 'Ubicaciones', 'se registro la Sucursal: 3', 41),
+(38, '2016-11-07', 'I', 'Categorias', 'Se registro la categoria: 4. ', 41),
+(39, '2016-11-07', 'I', 'Sub-Categorias', 'Se registro la Sub-Categoria: 3. ', 41),
+(40, '2016-11-07', 'I', 'Activos', 'se registro el activo: prueba1', 41),
+(41, '2016-11-12', 'I', 'Responsables', 'se registro el Responsable: 2', 41),
+(42, '2016-11-12', 'E', 'Responsables', 'se elimino el Responsable: 2', 41),
+(43, '2016-11-12', 'I', 'Responsables', 'se registro el Responsable: 2', 41),
+(44, '2016-11-12', 'E', 'Responsables', 'se elimino el Responsable: 2', 41),
+(45, '2016-11-12', 'I', 'Responsables', 'se registro el Responsable: 2', 41),
+(46, '2016-11-12', 'E', 'Responsables', 'se elimino el Responsable: 2', 41),
+(47, '2016-11-12', 'I', 'Responsables', 'se registro el Responsable: 2', 41),
+(48, '2016-11-12', 'E', 'Responsables', 'se elimino el Responsable: 2', 41),
+(49, '2016-11-12', 'I', 'Activos', 'se registro el activo: 1luyhq', 41),
+(50, '2016-11-12', 'E', 'Sucursales', 'se elimino la Sucursal: 2', 41),
+(51, '2016-11-12', 'I', 'Sucursales', 'se registro la Sucursal: 2', 41),
+(52, '2016-11-12', 'E', 'Categorias', 'Se elimino la categoria: 1. ', 41),
+(53, '2016-11-12', 'I', 'Categorias', 'Se registro la categoria: 5. ', 41),
+(54, '2016-11-12', 'E', 'Activos', 'se elimino el Activo: 5', 41),
+(55, '2016-11-12', 'E', 'Activos', 'se elimino el Activo: 4', 41),
+(56, '2016-11-12', 'E', 'Activos', 'se elimino el Activo: 3', 41),
+(57, '2016-11-12', 'E', 'Activos', 'se elimino el Activo: 2', 41),
+(58, '2016-11-13', 'I', 'Responsables', 'se registro el Responsable: 2', 41),
+(59, '2016-11-13', 'E', 'Responsables', 'se elimino el Responsable: 2', 41),
+(60, '2016-11-13', 'I', 'Responsables', 'se registro el Responsable: 2', 41),
+(61, '2016-11-13', 'E', 'Responsables', 'se elimino el Responsable: 2', 41),
+(62, '2016-11-13', 'I', 'Responsables', 'se registro el Responsable: 2', 41),
+(63, '2016-11-13', 'E', 'Responsables', 'se elimino el Responsable: 2', 41),
+(64, '2016-11-13', 'I', 'Sucursales', 'se registro la Sucursal: 3', 41),
+(65, '2016-11-13', 'E', 'Sucursales', 'se elimino la Sucursal: 3', 41),
+(66, '2016-11-13', 'I', 'Sucursales', 'se registro la Sucursal: 3', 41),
+(67, '2016-11-13', 'E', 'Sucursales', 'se elimino la Sucursal: 3', 41),
+(68, '2016-11-13', 'I', 'Activos', 'se registro el activo: 1luyhq', 41),
+(69, '2016-11-13', 'E', 'Activos', 'se elimino el Activo: 2', 41),
+(70, '2016-11-13', 'I', 'Activos', 'se registro el activo: 1luyhq', 41),
+(71, '2016-11-13', 'I', 'Categorias', 'Se registro la categoria: 5. ', 41),
+(72, '2016-11-13', 'E', 'Categorias', 'Se elimino la categoria: 5. ', 41),
+(73, '2016-11-13', 'I', 'Sub-Categorias', 'Se registro la Sub-Categoria: 4. ', 41),
+(74, '2016-11-13', 'E', 'Sub-Categoria', 'Se elimino la Sub-Categoria: 4. ', 41),
+(75, '2016-11-13', 'I', 'Sub-Categorias', 'Se registro la Sub-Categoria: 4. ', 41),
+(76, '2016-11-13', 'E', 'Sub-Categoria', 'Se elimino la Sub-Categoria: 4. ', 41);
 
 -- --------------------------------------------------------
 
@@ -167,10 +222,13 @@ INSERT INTO `pistasauditoria` (`idpistasAuditoria`, `fechaPista`, `tipo_operacio
 --
 
 DROP TABLE IF EXISTS `relacionactivos`;
-CREATE TABLE `relacionactivos` (
+CREATE TABLE IF NOT EXISTS `relacionactivos` (
   `idRelacionActivos` int(11) NOT NULL,
   `Activos_idActivos` int(11) NOT NULL,
-  `Resposable_idResposable` int(11) NOT NULL
+  `Resposable_idResposable` int(11) NOT NULL,
+  PRIMARY KEY (`idRelacionActivos`),
+  KEY `fk_RelacionActivos_Activos1_idx` (`Activos_idActivos`),
+  KEY `fk_RelacionActivos_Resposable1_idx` (`Resposable_idResposable`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -187,12 +245,14 @@ INSERT INTO `relacionactivos` (`idRelacionActivos`, `Activos_idActivos`, `Respos
 --
 
 DROP TABLE IF EXISTS `resposable`;
-CREATE TABLE `resposable` (
+CREATE TABLE IF NOT EXISTS `resposable` (
   `idResposable` int(11) NOT NULL,
   `Nombre` varchar(45) DEFAULT NULL,
   `Apellido` varchar(45) DEFAULT NULL,
   `Cedula` varchar(15) DEFAULT NULL,
-  `ubicacion_idUbicacion` int(11) NOT NULL
+  `ubicacion_idUbicacion` int(11) NOT NULL,
+  PRIMARY KEY (`idResposable`),
+  KEY `fk_resposable_ubicacion1_idx` (`ubicacion_idUbicacion`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -209,10 +269,12 @@ INSERT INTO `resposable` (`idResposable`, `Nombre`, `Apellido`, `Cedula`, `ubica
 --
 
 DROP TABLE IF EXISTS `subcategoria`;
-CREATE TABLE `subcategoria` (
+CREATE TABLE IF NOT EXISTS `subcategoria` (
   `idSubCategoria` int(11) NOT NULL,
   `Descripcion` varchar(45) NOT NULL,
-  `idCategoria` int(11) NOT NULL
+  `idCategoria` int(11) NOT NULL,
+  PRIMARY KEY (`idSubCategoria`),
+  KEY `Categoria_idCategoria` (`idCategoria`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -221,7 +283,8 @@ CREATE TABLE `subcategoria` (
 
 INSERT INTO `subcategoria` (`idSubCategoria`, `Descripcion`, `idCategoria`) VALUES
 (1, 'Laptop', 2),
-(2, 'Articulos de Oficina', 3);
+(2, 'Articulos de Oficina', 3),
+(3, 'Microondas', 4);
 
 -- --------------------------------------------------------
 
@@ -230,9 +293,10 @@ INSERT INTO `subcategoria` (`idSubCategoria`, `Descripcion`, `idCategoria`) VALU
 --
 
 DROP TABLE IF EXISTS `sucursal`;
-CREATE TABLE `sucursal` (
+CREATE TABLE IF NOT EXISTS `sucursal` (
   `idsucursal` int(11) NOT NULL,
-  `Descripcion` varchar(45) NOT NULL
+  `Descripcion` varchar(45) NOT NULL,
+  PRIMARY KEY (`idsucursal`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -241,8 +305,7 @@ CREATE TABLE `sucursal` (
 
 INSERT INTO `sucursal` (`idsucursal`, `Descripcion`) VALUES
 (1, 'Region Occidente'),
-(2, 'Region Centro-Occidente'),
-(3, 'RegiÃ³n Andina');
+(2, 'Region Centro-Occidente');
 
 -- --------------------------------------------------------
 
@@ -251,10 +314,12 @@ INSERT INTO `sucursal` (`idsucursal`, `Descripcion`) VALUES
 --
 
 DROP TABLE IF EXISTS `tipo_usuario`;
-CREATE TABLE `tipo_usuario` (
+CREATE TABLE IF NOT EXISTS `tipo_usuario` (
   `idTipo_Usuario` int(11) NOT NULL,
   `descripcion` varchar(45) DEFAULT NULL,
-  `modulos_idmodulos` int(11) NOT NULL
+  `modulos_idmodulos` int(11) NOT NULL,
+  PRIMARY KEY (`idTipo_Usuario`),
+  KEY `fk_Tipo_Usuario_modulos1_idx` (`modulos_idmodulos`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -272,10 +337,12 @@ INSERT INTO `tipo_usuario` (`idTipo_Usuario`, `descripcion`, `modulos_idmodulos`
 --
 
 DROP TABLE IF EXISTS `ubicacion`;
-CREATE TABLE `ubicacion` (
+CREATE TABLE IF NOT EXISTS `ubicacion` (
   `idUbicacion` int(11) NOT NULL,
   `Descripcion` varchar(45) DEFAULT NULL,
-  `sucursal_idsucursal` int(11) NOT NULL
+  `sucursal_idsucursal` int(11) NOT NULL,
+  PRIMARY KEY (`idUbicacion`),
+  KEY `fk_Ubicacion_sucursal_idx` (`sucursal_idsucursal`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -284,7 +351,8 @@ CREATE TABLE `ubicacion` (
 
 INSERT INTO `ubicacion` (`idUbicacion`, `Descripcion`, `sucursal_idsucursal`) VALUES
 (1, 'Administracion Occidente', 1),
-(2, 'TI Occidente', 1);
+(2, 'TI Occidente', 1),
+(3, 'Base', 1);
 
 -- --------------------------------------------------------
 
@@ -293,16 +361,18 @@ INSERT INTO `ubicacion` (`idUbicacion`, `Descripcion`, `sucursal_idsucursal`) VA
 --
 
 DROP TABLE IF EXISTS `usuarios`;
-CREATE TABLE `usuarios` (
-  `idUsuarios` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `idUsuarios` int(11) NOT NULL AUTO_INCREMENT,
   `usuario` varchar(45) DEFAULT NULL,
   `clave` varchar(100) DEFAULT NULL,
   `Nombre` varchar(45) DEFAULT NULL,
   `Apellido` varchar(45) DEFAULT NULL,
   `Cedula` varchar(15) DEFAULT NULL,
   `foto` varchar(255) DEFAULT NULL,
-  `Tipo_Usuario_idTipo_Usuario` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `Tipo_Usuario_idTipo_Usuario` int(11) NOT NULL,
+  PRIMARY KEY (`idUsuarios`),
+  KEY `fk_Usuarios_Tipo_Usuario1_idx` (`Tipo_Usuario_idTipo_Usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `usuarios`
@@ -312,102 +382,6 @@ INSERT INTO `usuarios` (`idUsuarios`, `usuario`, `clave`, `Nombre`, `Apellido`, 
 (41, 'admin', 'admin', 'Carlos', 'Bermudez', 'V-15.060.580', 'carlos.jpg', 0),
 (42, 'dchavez', '123', 'Daniel', 'Chavez', '2398334', 'default.jpg', 1);
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `activos`
---
-ALTER TABLE `activos`
-  ADD PRIMARY KEY (`idActivos`),
-  ADD KEY `fk_activos_ubicacion1_idx` (`ubicacion_idUbicacion`),
-  ADD KEY `Categoria_idCategoria` (`categorias_idCategoria`),
-  ADD KEY `idSubCategoria` (`idSubCategoria`);
-
---
--- Indices de la tabla `categorias`
---
-ALTER TABLE `categorias`
-  ADD PRIMARY KEY (`idCategoria`);
-
---
--- Indices de la tabla `mapas_acceso`
---
-ALTER TABLE `mapas_acceso`
-  ADD PRIMARY KEY (`idmapas_acceso`);
-
---
--- Indices de la tabla `modulos`
---
-ALTER TABLE `modulos`
-  ADD PRIMARY KEY (`idmodulos`),
-  ADD KEY `fk_modulos_mapas_acceso1_idx` (`mapas_acceso_idmapas_acceso`);
-
---
--- Indices de la tabla `pistasauditoria`
---
-ALTER TABLE `pistasauditoria`
-  ADD PRIMARY KEY (`idpistasAuditoria`);
-
---
--- Indices de la tabla `relacionactivos`
---
-ALTER TABLE `relacionactivos`
-  ADD PRIMARY KEY (`idRelacionActivos`),
-  ADD KEY `fk_RelacionActivos_Activos1_idx` (`Activos_idActivos`),
-  ADD KEY `fk_RelacionActivos_Resposable1_idx` (`Resposable_idResposable`);
-
---
--- Indices de la tabla `resposable`
---
-ALTER TABLE `resposable`
-  ADD PRIMARY KEY (`idResposable`),
-  ADD KEY `fk_resposable_ubicacion1_idx` (`ubicacion_idUbicacion`);
-
---
--- Indices de la tabla `subcategoria`
---
-ALTER TABLE `subcategoria`
-  ADD PRIMARY KEY (`idSubCategoria`),
-  ADD KEY `Categoria_idCategoria` (`idCategoria`);
-
---
--- Indices de la tabla `sucursal`
---
-ALTER TABLE `sucursal`
-  ADD PRIMARY KEY (`idsucursal`);
-
---
--- Indices de la tabla `tipo_usuario`
---
-ALTER TABLE `tipo_usuario`
-  ADD PRIMARY KEY (`idTipo_Usuario`),
-  ADD KEY `fk_Tipo_Usuario_modulos1_idx` (`modulos_idmodulos`);
-
---
--- Indices de la tabla `ubicacion`
---
-ALTER TABLE `ubicacion`
-  ADD PRIMARY KEY (`idUbicacion`),
-  ADD KEY `fk_Ubicacion_sucursal_idx` (`sucursal_idsucursal`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`idUsuarios`),
-  ADD KEY `fk_Usuarios_Tipo_Usuario1_idx` (`Tipo_Usuario_idTipo_Usuario`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `idUsuarios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 --
 -- Restricciones para tablas volcadas
 --
